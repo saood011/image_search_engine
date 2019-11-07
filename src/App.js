374bc07a9c4c14a2ed96553dc7fb9fd5378cb394
saood <saood011@yahoo.com>
2019-11-07 15:11:14 +0100

    previous and next button add with spinner

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [term, setTerm] = useState("car");
   const [pictures, setPictures] = useState([]);
   const [pageNum, setPageNum] = useState(2);
+  const [isLoading, setIsLoading] = useState(false);
 
   const changeHandler = e => {
     setTerm(e.target.value);
@@ -16,21 +17,23 @@ function App() {
 
   const sendRequest = e => {
     e.preventDefault();
+    setIsLoading(true);
+
     axios
       .get("https://api.unsplash.com/search/photos", {
         params: {
           query: term,
-          page: 1,
-          per_page: 15
+          per_page: 24
         },
         headers: {
           Authorization:
-            "Client-ID 89a74f8c26da940b295f7c22ccaf83e3404ac033065c8db15fcbbc3b0639a400"
+            "Client-ID 082136f9e5e99ecb9cd2578e0d9a0b7b548e18001db11fd752f7c9e5abba096f"
         }
       })
       .then(response => {
         setPictures([...response.data.results]);
-        console.log(response.data.results);
+        console.log(response.data);
+        setIsLoading(false);
       })
       .catch(error => {
         console.log(error.message);
@@ -40,21 +43,24 @@ function App() {
   /// sending request on next/previous button click
 
   const changePage = e => {
+    setIsLoading(true);
+
     axios
       .get("https://api.unsplash.com/search/photos", {
         params: {
           query: term,
           page: pageNum,
-          per_page: 15
+          per_page: 24
         },
         headers: {
           Authorization:
-            "Client-ID 89a74f8c26da940b295f7c22ccaf83e3404ac033065c8db15fcbbc3b0639a400"
+            "Client-ID 082136f9e5e99ecb9cd2578e0d9a0b7b548e18001db11fd752f7c9e5abba096f"
         }
       })
       .then(response => {
         setPictures([...response.data.results]);
         console.log(response.data.results);
+        setIsLoading(false);
       })
       .catch(error => {
         console.log(error.message);
@@ -87,6 +93,29 @@ function App() {
             </form>
           </div>
         </div>
+      ) : isLoading ? (
+        <div>
+          <div className="cover"></div>
+          <form
+            onSubmit={sendRequest}
+            className="text-center d-flex mt-2 shadow"
+          >
+            <input
+              type="text"
+              onChange={changeHandler}
+              className="form-control"
+              placeholder=" Enter the name of the picture..."
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary ml-1"
+            />
+          </form>
+          <div class="text-center">
+            <div class="lds-hourglass"></div>
+          </div>
+        </div>
       ) : (
         <div>
           <div className="cover"></div>
@@ -106,29 +135,32 @@ function App() {
               className="btn btn-primary ml-1"
             />
           </form>
+
           <div className="d-flex flex-wrap justify-content-center mt-2 list align-items-center">
-            {pictures.length ? (
-              pictures.map(pic => (
-                <div className="card m-2 p-1 shadow" key={pic.id}>
-                  <a href={pic.urls.full} target="blank">
-                    {" "}
-                    <img
-                      src={pic.urls.thumb}
-                      className="card-img-top images"
-                      alt={pic.id}
-                    />
-                  </a>
-                </div>
-              ))
-            ) : (
-              <h3 className="text-dark">Search result here...</h3>
-            )}
+            {pictures.length
+              ? pictures.map(pic => (
+                  <div className="card m-2 p-1 shadow" key={pic.id}>
+                    <a href={pic.urls.full} target="blank">
+                      {" "}
+                      <img
+                        src={pic.urls.thumb}
+                        className="card-img-top images"
+                        alt={pic.id}
+                      />
+                    </a>
+                  </div>
+                ))
+              : null}
           </div>
+
           <div className="d-flex justify-content-between">
             <button
               className="btn btn-danger"
               onClick={() => {
-                setPageNum(pageNum - 1);
+                if (pageNum > 1) {
+                  setPageNum(pageNum - 1);
+                }
+
                 console.log(pageNum);
                 changePage();
               }}
@@ -148,6 +180,7 @@ function App() {
           </div>
         </div>
       )}
+      <footer> &copy; 2019 Designed by Saood using React</footer>
     </div>
   );
 }
